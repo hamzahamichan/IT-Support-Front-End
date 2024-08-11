3bba87aef6addb4166774b2abd71639e041f38c7
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-11 17:05:51 +0100

    importation des modules

--- ITAppSupport/src/app/app.module.ts
+++ ITAppSupport/src/app/app.module.ts
@@ -7,18 +7,41 @@ import { EquipementComponent } from './equipement/equipement.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {HttpClientModule} from "@angular/common/http";
 import {ReactiveFormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import { TicketSupportComponent } from './ticket-support/ticket-support.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatNativeDateModule, MatOptionModule} from "@angular/material/core";
+import {MatSelectModule} from "@angular/material/select";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import { LoginComponent } from './login/login.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EquipementComponent,
-    NavBarComponent
+    NavBarComponent,
+    TicketSupportComponent,
+    LoginComponent,
+    AdminTemplateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatToolbarModule
 
   ],
   providers: [],
