5b41388c063f2bf0b875a8b596bb115f743a950f
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-11 17:06:30 +0100

    configuration des routes

--- ITAppSupport/src/app/app-routing.module.ts
+++ ITAppSupport/src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {NavBarComponent} from "./nav-bar/nav-bar.component";
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { EquipementComponent } from './equipement/equipement.component';
+import { TicketSupportComponent } from './ticket-support/ticket-support.component';
 
 const routes: Routes = [
-  {path:"navbar",component:NavBarComponent}
+  { path: 'home', component: HomeComponent }, // Page d'accueil par défaut
+  { path: 'login', component: LoginComponent },
+  { path: 'admin', component: AdminTemplateComponent, children: [
+      { path: 'navbar', component: NavBarComponent },
+      { path: 'equipementList', component: EquipementComponent },
+      { path: 'ticket', component: TicketSupportComponent },
+      { path: '', redirectTo: 'navbar', pathMatch: 'full' }
+    ]
+  },
+  { path: '**', redirectTo: '' } // Redirection pour les routes non trouvées
 ];
 
 @NgModule({
