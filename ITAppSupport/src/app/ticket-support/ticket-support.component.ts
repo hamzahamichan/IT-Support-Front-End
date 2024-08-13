ca058184daae53f13acbdc0adf777ca0929ab4fd
hamzahamzahamzahm <hamzahamichan159@gmail.com>
2024-08-13 12:12:05 +0100

    création de logique d'ajouter un ticket

--- ITAppSupport/src/app/ticket-support/ticket-support.component.ts
+++ ITAppSupport/src/app/ticket-support/ticket-support.component.ts
@@ -3,7 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TicketSupportService } from '../services/ticket-support.service';
 import { EquipementService } from '../services/equipement.service';
 import { Equipement } from '../Model/equipement.model';
-import {Ticket} from "../Model/Ticket.model";
+import { Panne } from '../Model/panne.model';
+import { Ticket } from "../Model/Ticket.model";
+import { PanneserviceService } from "../services/panneservice.service";
 
 @Component({
   selector: 'app-ticket-support',
@@ -13,28 +15,39 @@ import {Ticket} from "../Model/Ticket.model";
 export class TicketSupportComponent implements OnInit {
   ticketForm: FormGroup;
   equipements: Equipement[] = [];
-  statuts = Object.values(Ticket); // Utilisation de l'énumération Tichet pour les statuts
+  pannes: Panne[] = [];
+  etat = Object.values(Ticket);
 
   constructor(
     private ticketService: TicketSupportService,
     private equipementService: EquipementService,
+    private panneService: PanneserviceService,
     private fb: FormBuilder
   ) {
     this.ticketForm = this.fb.group({
       description: ['', Validators.required],
       date_creation: [new Date(), Validators.required],
-      statut: ['', Validators.required], // Renommé pour plus de clarté
+      etat: ['', Validators.required],
       id_equipement: ['', Validators.required],
+      panne: ['', Validators.required]
     });
   }
 
   ngOnInit(): void {
     this.equipementService.getEquipement().subscribe((equipements) => (this.equipements = equipements));
+    this.panneService.getPannes().subscribe((pannes) => (this.pannes = pannes));
   }
 
   onSubmit(): void {
     if (this.ticketForm.valid) {
-      const newTicket: Ticket = this.ticketForm.value;
+      const ticketData = this.ticketForm.value;
+
+      const newTicket: Ticket = {
+        ...ticketData,
+        panne: { id_panne: ticketData.panne.id_panne },  // Format correct pour le backend
+        id_equipement: ticketData.id_equipement
+      };
+
       this.ticketService.createTicket(newTicket).subscribe(response => {
         console.log('Ticket créé avec succès', response);
       });
